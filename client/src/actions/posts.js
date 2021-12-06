ac4b5a46956ab5cf600a447323d3976fccf2281e
Akshay Dodkade <akshaydodkade@gmail.com>
2021-12-06 21:24:44 +0530

    memories - initial commit

---
+++ client/src/actions/posts.js
@@ -0,0 +1,20 @@
+import * as api from "../api";
+
+// action creators
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({type: 'FETCH_ALL', payload: data});
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export const createPost = (newPost) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(newPost);
+    dispatch({type: 'CREATE', payload: data});
+  } catch (error) {
+    console.log(error.message);
+  }
+}
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import useStyles from "./styles";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
+
+const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedFile: ''
+  });
+
+  const classes = useStyles();
+
+  const handleSubmit = () => {
+
+  };
+
+  const clear = () => {
+
+  }
+
+  return (
+    <Paper>
+      <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({...postData, creator: e.target.value})} ></TextField>
+        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({...postData, title: e.target.value})} ></TextField>
+        <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({...postData, message: e.target.value})} ></TextField>
+        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({...postData, tags: e.target.value})} ></TextField>
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({base64}) => setPostData({...postData, selectedFile: base64})}
+          >
+          </FileBase>
+        </div>
+        <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Add New Memory</Button>
+        <Button variant="contained" color="secondary" size="small" fullWidth onClick={clear}>Cancel</Button>
+      </form>
+    </Paper>
+  );
+}
+
+export default Form;

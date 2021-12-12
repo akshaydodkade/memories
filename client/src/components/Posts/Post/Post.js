3b079a4e90422d7ead8b7db72ba448019646e88d
Akshay Dodkade <akshaydodkade@gmail.com>
2021-12-12 19:36:02 +0530

    optimization - added action type const for client reducers and actions

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -30,7 +30,7 @@ const Post = ({ post, currentId, setCurrentId }) => {
       </div>
       <Typography className={classes.title} variant="h6">{post.title}</Typography>
       <CardContent>
-        <Typography variant="body2" gutterBottom>{post.message}</Typography>
+        <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" onClick={() => { dispatch(likePost(post._id)) }}>
